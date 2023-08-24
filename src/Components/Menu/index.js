1c97663b2fade5685192f8acbadb00c8db6ffc35
Luiz Marangoni <lzmarangoni@gmail.com>
2023-08-24 14:47:48 -0300

    ajuste na fonte de titulo

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -4,14 +4,14 @@ import { HiMenu } from "react-icons/hi";
 
 
 export default function Menu({itens}) {
-    const [abreFecha, setAbreFecha] = useState(true)
+    const [abreFecha, setAbreFecha] = useState(false)
     const trueEfalse = () => {
         setAbreFecha(!abreFecha)
         console.log(abreFecha)
     }
     return (
         <> 
-         <HiMenu className={styles.menuMobile} size={50} onClick={trueEfalse}></HiMenu> 
+        <HiMenu className={styles.menuMobile} size={40} onClick={trueEfalse}></HiMenu> 
         <nav className={abreFecha? styles.menu : styles.menuOff}>
         
                 {itens.map(item => { return <a onClick={trueEfalse} key={item.id} href={item.id}>{item.nome}</a> })}
--- src/Components/Sobre/index.js
+++ src/Components/Sobre/index.js
@@ -7,15 +7,17 @@ import Titulo from '../Titulo'
 export default function Sobre() {
   return (
     <div className={styles.content}>
-      <img className={styles.image} src={image} alt="" />
+      
       <div className={styles.info}>
         <Titulo>Sobre</Titulo>
+        <img className={styles.image} src={image} alt="" />
         <p className={styles.text}>
           Psicóloga clínica, experiência de 20 anos, graduada em Psicologia,
           Especialista em Psicologia Hospitalar, (Hospital Sta paula-UniSP)
           Especialista em psico-oncologia, Lgoterapeuta. Atendimento de
           adolescentes e adultos de atuo como Consultora de Saúde Ocupacional
         </p>
+        
         <Button id={styles.botaoMain}>Agende sua consulta</Button>
       </div>
     </div>
