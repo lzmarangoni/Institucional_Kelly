bfc18e4c8088293363e98bcd7cfd1ec5a5c2da53
Luiz Marangoni <lzmarangoni@gmail.com>
2023-08-28 23:35:27 -0300

    ajuste no card principal

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from "react";
+import styles from "./Card.module.css";
 
-export default function Card({imagem, titulo, texto}) {
+export default function Card({ data }) {
   return (
     <div className={styles.card}>
-        <img className={styles.image} src={imagem} alt=''/>
-        <h3 className={styles.title}>{titulo}</h3>
-        <p className={styles.texto}>{texto}</p>
+      <div className={styles.simbolo}>
+        {data.simbolo}
+        <h3 className={styles.titulo}>{data.titulo}</h3>
+      </div>
+      <p className={styles.texto}>{data.texto}</p>
     </div>
-  )
+  );
 }
--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,43 +1,63 @@
 import React from "react";
 import styles from "./Main.module.css";
 import Card from "../Card";
-import atdOnline from "./Atendimento Online.png";
-import atdPresencial from "./Atendimento Presencial.png";
-import Titulo from '../Titulo'
+import { HiClock, HiHome, HiMap, HiChatAlt2 } from "react-icons/hi";
+
+import Titulo from "../Titulo";
 
 export default function Main() {
+  const beneficios = [
+    {
+      simbolo: <HiClock size={40}/>,
+      titulo: "Flexibilidade",
+      texto:
+        "A terapia online oferece maior flexibilidade de horários, permitindo que os pacientes escolham horários que sejam mais convenientes para eles.",
+    },
+    {
+      simbolo: <HiHome size={40}/>,
+      titulo: "Comodidade",
+      texto:
+        "Os pacientes podem realizar a terapia no conforto do local de sua escolha, o que pode criar uma sensação de segurança e relaxamento durante as sessões terapêuticas.",
+    },
+    {
+      simbolo: <HiMap size={40}/>,
+      titulo: "Acessibilidade",
+      texto:
+        "A terapia online elimina a necessidade de deslocamento, economizando tempo e custos associados a transporte. Além disso, possibilita o acesso a profissionais qualificados, mesmo que não haja serviços de psicologia disponíveis localmente.",
+    },
+    {
+      simbolo: <HiChatAlt2 size={40}/>,
+      titulo: "Continuidade",
+      texto:
+        "A terapia online permite que os pacientes continuem o tratamento, mesmo quando estão viajando a negócios, de férias ou enfrentando situações que dificultariam a participação nas sessões presenciais.",
+    },
+  ];
   return (
     <div className={styles.container}>
-    <Titulo>Terapia e movimento</Titulo>
-    <div className={styles.main}>
-      
-      <div className={styles.conteudo}>
-        
-        <p className={styles.text}>
-
-          Quero compartilhar com vocês desse movimento de transformação que a
-          terapia gera na gente. Como diz a primeira lei de Newton: 
+      <Titulo>Terapia e movimento</Titulo>
+      <div className={styles.main}>
+        <div className={styles.conteudo}>
           <p className={styles.text}>
-          "um corpo em movimento, tende a permanecer em movimento"
+            Quero compartilhar com vocês desse movimento de transformação que a
+            terapia gera na gente. Como diz a primeira lei de Newton:
+            <p className={styles.text}>
+              "um corpo em movimento, tende a permanecer em movimento"
+            </p>
+            <p className={styles.text}>
+              Sim isso é física, mas tambem tem muito a ver com a frase que
+              falei anteriormente.
+            </p>
+            A terapia gera um movimento, o movimento de sair de si, de sair do
+            ponto A para o ponto B, porém quando chegamos ao ponto B percebemos
+            que o percurso ainda não acabou, e isso tende a nos fazer permanecer
+            em movimento. Mas um movimento de transformação diária, de
+            autoconhecimento, e desenvolvimento interpessoal.
           </p>
-        
-          <p className={styles.text}>
-          Sim isso é física, mas tambem tem muito a ver com a frase que falei anteriormente. 
-          </p>
-          A terapia gera um movimento, o movimento de sair de si, de sair do ponto A para
-          o ponto B, porém quando chegamos ao ponto B percebemos que o percurso
-          ainda não acabou, e isso tende a nos fazer permanecer em movimento.
-          Mas um movimento de transformação diária, de autoconhecimento, e
-          desenvolvimento interpessoal. 
-        </p>
         </div>
         <div className={styles.cards}>
-        
-          <Card titulo="Atendimento 100% Online" imagem={atdOnline} texto=""></Card>
+          {beneficios.map(beneficio => {return <Card data={beneficio}/>}) }
         </div>
-      
+      </div>
     </div>
-    </div>
-    
   );
 }
